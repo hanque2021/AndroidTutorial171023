fd12158f5cb728c12873d21bfd76839048599a13
Your Name <you@example.com>
2017-10-24 15:07:56 +0900

    c

--- T07_thread/app/src/main/java/com/example/a/t07_thread/MainActivity.java
+++ T07_thread/app/src/main/java/com/example/a/t07_thread/MainActivity.java
@@ -1,21 +1,40 @@
 package com.example.a.t07_thread;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  static  final  int MYVALUE = 100;
     class MyThread extends  Thread{
         @Override
         public void run() {
             for(int i=0; i<100000;i++){
                 Log.d("thread", "count:"+i);
                 //myButton.setText("count "+i);  //Thread 에서 기본적으로 UI접근 못한다. 실행 시 에러.
+                Message msg = new Message();
+                msg.what =MYVALUE;
+                msg.arg1 = i;
+
+                handler.sendMessage(msg);
              }
         }
     }
+
+    Handler handler = new Handler() {
+        @Override
+        public void  handleMessage(Message msg){
+            //super.handleMessage(msg);
+            myButton.setText("count:"+msg.arg1);
+        }
+
+    };
+
     Button myButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
