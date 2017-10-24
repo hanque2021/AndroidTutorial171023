8a712876d13157783738e02165db141bce3dc7e9
Your Name <you@example.com>
2017-10-24 11:08:18 +0900

    Rejected commit: Default

--- T03_activity/app/src/main/java/com/example/a/t03_activity/MainActivity.java
+++ T03_activity/app/src/main/java/com/example/a/t03_activity/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_REQ = 100; //어디에서 가져오는 정의하기 위해
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +17,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtnClick(View v){
         Intent intent = new Intent(this,MyActivity.class);
-        startActivity(intent);
+        //intent에 값 전달
+        intent.putExtra("id", "abcd");
+        intent.putExtra("pw", "1234");
+        intent.putExtra("level",1);
+
+        //startActivity(intent);   //결과값 안 받는 경우
+        startActivityForResult(intent,MY_REQ);   //결과값 받는 경우, 이 액티비티의 값을 전달
     }
+
+    //다른 activity에서 값 넘겨 받는 경우
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+       // super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==MY_REQ){
+            if(resultCode == RESULT_OK){
+                String str = data.getStringExtra("myResult");
+                Toast.makeText(this, "str:"+str, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 
 }
--- T03_activity/app/src/main/java/com/example/a/t03_activity/MyActivity.java
+++ T03_activity/app/src/main/java/com/example/a/t03_activity/MyActivity.java
@@ -1,7 +1,9 @@
 package com.example.a.t03_activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
@@ -10,6 +12,22 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        //입력한 값 얻어오기. 형식에 맞는 extra얻어오기
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("id");
+        String pw = intent.getStringExtra("pw");
+        int level = intent.getIntExtra("level", 0);   //int는 default값 넣어줘야
+    }
+
+    public void onFinishClick(View v){
+        Intent intent = new Intent();
+        intent.putExtra("myResult", "abdcd");
+        intent.putExtra("myValue", 1233);
+
+        setResult(RESULT_OK, intent);
+
+        finish();
     }
 
     @Override
