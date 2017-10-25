49b5c9b5408a727d916b7ae2461ed12963172716
Your Name <you@example.com>
2017-10-25 10:26:12 +0900

    d

--- P01_weather/app/src/main/java/com/example/a/p01_weather/MainActivity.java
+++ P01_weather/app/src/main/java/com/example/a/p01_weather/MainActivity.java
@@ -4,6 +4,14 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -13,6 +21,7 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
     class WeatherData{
         int day;
         int hour;
@@ -26,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+
 
             for(int i=0; i<weatherList.size(); i++){
                 Log.d("test", ""+weatherList.get(i).temp);
@@ -89,13 +99,69 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    class  MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return weatherList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return weatherList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup viewGroup) {
+            if(convertView==null){
+                LayoutInflater inf = getLayoutInflater();
+                convertView = inf.inflate(R.layout.item_view,null);
+            }
+            WeatherData data = weatherList.get(position);
+
+            TextView tvTitle = (TextView) convertView.findViewById(R.id.item_title);
+            TextView tvDesc = (TextView) convertView.findViewById(R.id.item_desc);
+            ImageView itemIcon = convertView.findViewById(R.id.itemIcon);
+
+            tvTitle.setText(data.temp+"도 "+data.wfKor  );
+            tvDesc.setText(data.day+"일 "+data.hour+"시" );
+            //itemIcon.setImageResource(data.itemIcon);
+
+            //TextView tvDay = (TextView) convertView.findViewById(R.id.item_day);
+            //TextView tvHour = (TextView) convertView.findViewById(R.id.item_hour);
+            //TextView tvTemp = (TextView) convertView.findViewById(R.id.item_temp);
+            //TextView tvWfKor = (TextView) convertView.findViewById(R.id.item_wfkor);
+            //ImageView itemIcon = convertView.findViewById(R.id.itemIcon);
+
+            //tvDay.setText(myData.day);
+            //tvHour.setText(myData.hour);
+            //tvTemp.setText(myData.temp);
+            //tvWfKor.setText(myData.wfKor);
+            //itemIcon.setImageResource(myData.temp);
+
+            return convertView;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        MyAdapter adapter = new MyAdapter();
+        listView.setAdapter(adapter);
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
         MyPullParser task = new MyPullParser();
         task.execute("http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1153052000");
+
     }
 
 }
