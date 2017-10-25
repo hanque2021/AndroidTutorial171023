1fa29d2d9a2c4a5972e0f015b8f1a875a12b4f9c
Your Name <you@example.com>
2017-10-25 18:02:37 +0900

    init

--- T16_Service2/app/src/main/java/com/example/a/t16_service2/MainActivity.java
+++ T16_Service2/app/src/main/java/com/example/a/t16_service2/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.a.t16_service2;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     MyService mySerive;
+    ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            MyService.MyBinder binder = (MyService.MyBinder) iBinder;
+            mySerive = binder.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +36,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Intent intent = new Intent(this, MyService.class);
-       // bindService(intent,);
+        bindService(intent,connection, BIND_AUTO_CREATE);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-      //  unbindService();
+        unbindService(connection);
     }
 
     public void onBtnClick(View v){
