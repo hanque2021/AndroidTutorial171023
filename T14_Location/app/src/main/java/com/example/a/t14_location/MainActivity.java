3664c29428f580d7f6a4b464950571414271869a
Your Name <you@example.com>
2017-10-25 16:16:57 +0900

    init

--- T14_Location/app/src/main/java/com/example/a/t14_location/MainActivity.java
+++ T14_Location/app/src/main/java/com/example/a/t14_location/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a.t14_location;
 
 import android.Manifest;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -17,20 +19,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String[] p = {Manifest.permission.ACCESS_FINE_LOCATION};
-                //requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION});
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+        //requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION});
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION};
-            requestPermissions(permission,0);
+            requestPermissions(permission, 0);
         }
         String res = "";
-        TextView textView = (TextView) findViewById(R.id.textView);
+        final TextView textView = (TextView) findViewById(R.id.textView);
         LocationManager manager = (LocationManager) getSystemService(LOCATION_SERVICE);
         List<String> providers = manager.getAllProviders();
-        for(int i=0;i<providers.size();i++){
+        for (int i = 0; i < providers.size(); i++) {
             //textView.append(providers.get(i));
-            res +="provider:"+providers.get(i)+" status:"+manager.isProviderEnabled(providers.get(i))+"\n";
+            res += "provider:" + providers.get(i) + " status:" + manager.isProviderEnabled(providers.get(i)) + "\n";
 
         }
         textView.setText(res);
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                String str = "Lat: "+ location.getLatitude()+
+                        "log:"+location.getLongitude()+"\n";
+                textView.append(str);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);  //disable inspect 선택..
     }
 }
