a7a4a6d22778a12ee2f620dd1115474429d72119
Your Name <you@example.com>
2017-10-25 11:05:47 +0900

    d

--- T10_mediaplayer/app/src/main/java/com/example/a/t10_mediaplayer/MainActivity.java
+++ T10_mediaplayer/app/src/main/java/com/example/a/t10_mediaplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.a.t10_mediaplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Runtime permission에 대해서 권한 부여 물어보는 코드 삽입. deny한 경우 설정>앱가서 변경해야 함.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { //M이 마시멜로 나타냄.
+            String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+            requestPermissions(permissions,0);
+        }
+
+
+    }
+    public  void onPlayClick(View v){
+
+    }
+
+    public  void onStopClick(View v){
+
     }
 }
