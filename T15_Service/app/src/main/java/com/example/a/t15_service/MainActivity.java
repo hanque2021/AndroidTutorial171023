f65d47f0f5fb03ad64960f90701df4aaafb52942
Your Name <you@example.com>
2017-10-25 17:40:05 +0900

    init

--- T15_Service/app/src/main/java/com/example/a/t15_service/MainActivity.java
+++ T15_Service/app/src/main/java/com/example/a/t15_service/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.a.t15_service;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public  void onStartClick(View v){
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
+
+        //MyIntentService.
+    }
+
+    public  void onStopClick(View v){
+        Intent intent = new Intent(this, MyService.class);
+        stopService(intent);
     }
 }
---
+++ T15_Service/app/src/main/java/com/example/a/t15_service/MyService.java
@@ -0,0 +1,40 @@
+package com.example.a.t15_service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("service", "onCreate");
+
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent,  int flags, int startId) {
+        Log.d("service", "onStartCommand");
+        stopSelf();
+        return  START_NOT_STICKY;
+        //return super.onStartCommand(intent, flags, startId);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("service", "onDestory");
+    }
+}
