e6d05853d596757d53c4b0183307eff66ec40988
Your Name <you@example.com>
2017-10-25 11:50:13 +0900

    d

--- T10_mediaplayer/app/src/main/java/com/example/a/t10_mediaplayer/MainActivity.java
+++ T10_mediaplayer/app/src/main/java/com/example/a/t10_mediaplayer/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.a.t10_mediaplayer;
 
 import android.Manifest;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.SeekBar;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    SeekBar seekBar = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
 
         //Runtime permission에 대해서 권한 부여 물어보는 코드 삽입. deny한 경우 설정>앱가서 변경해야 함.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { //M이 마시멜로 나타냄.
@@ -21,11 +28,45 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    MediaPlayer mp = null;
     public  void onPlayClick(View v){
+        String path = Environment.getExternalStorageDirectory() + "/Download/Kalimba.mp3";
+        mp = new MediaPlayer();
+        //mp.getCurrentPosition();
+        try {
+            mp.setDataSource(path);
+            mp.prepare();
+            mp.start();
+            seekBar.setMax(mp.getDuration());
+            Thread th = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (mp!=null){
+                        try {
+                            seekBar.setProgress(mp.getCurrentPosition());
+                        }catch (Exception e){
+                            seekBar.setProgress(0);
+                        }
+                    }
+                }
+            });
+            th.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //File f = new File(path);
+        //Toast.makeText(this, "file exists:"+f.exists(), Toast.LENGTH_SHORT).show();
 
     }
 
     public  void onStopClick(View v){
-
+        if(mp!=null){
+            mp.stop();
+            mp.release();
+            mp = null;
+        }
     }
 }
