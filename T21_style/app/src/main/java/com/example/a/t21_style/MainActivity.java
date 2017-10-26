95aa4d023477935ff245892e4baa990b8e33aafa
Your Name <you@example.com>
2017-10-26 15:59:16 +0900

    init

--- T21_style/app/src/main/java/com/example/a/t21_style/MainActivity.java
+++ T21_style/app/src/main/java/com/example/a/t21_style/MainActivity.java
@@ -4,23 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView selectedTextView, workingTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        selectedTextView = (TextView) findViewById(R.id.selected_textView);
-        workingTextView = (TextView) findViewById(R.id.working_textView);
-
-        Button btnZero = (Button) findViewById(R.id.btnZero);
-        Button btnOne = (Button) findViewById(R.id.btnOne);
-        Button btnEnter = (Button) findViewById(R.id.btnEnter);
 
         View.OnClickListener numberListener = new View.OnClickListener() {
             @Override
@@ -32,22 +26,47 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     workingTextView.setText(working+btn.getText());
                 }
-                //int number = Integer.parseInt(btn.getText().toString());
+
             }
         };
 
-        btnZero.setOnClickListener(numberListener);
-        btnOne.setOnClickListener(numberListener);
+        selectedTextView = (TextView) findViewById(R.id.selected_textView);
+        workingTextView = (TextView) findViewById(R.id.working_textView);
 
-        btnEnter.setOnClickListener(new View.OnClickListener() {
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.remote_control_tablelayout);
+
+        int number = 1;
+        for(int i=2; i<tableLayout.getChildCount()-1; i++){
+            TableRow row = (TableRow) tableLayout.getChildAt(i);
+            for(int k=0; k<row.getChildCount(); k++){
+                Button btn = (Button) row.getChildAt(k);
+                btn.setText(""+number);
+                btn.setOnClickListener(numberListener);
+                number++;
+            }
+        }
+        TableRow row = (TableRow) tableLayout.getChildAt( tableLayout.getChildCount()-1 );
+        Button deleteButton = (Button) row.getChildAt(0);
+        Button zeroButton = (Button) row.getChildAt(1);
+        Button enterButton = (Button) row.getChildAt(2);
+
+        zeroButton.setOnClickListener(numberListener);
+        zeroButton.setText("0");
+        deleteButton.setText("delete");
+        deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                selectedTextView.setText(workingTextView.getText());
+                workingTextView.setText("0");
+            }
+        });
+
+        enterButton.setText("enter");
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedTextView.setText( workingTextView.getText() );
                 workingTextView.setText("0");
             }
         });
     }
-
-
-
 }
