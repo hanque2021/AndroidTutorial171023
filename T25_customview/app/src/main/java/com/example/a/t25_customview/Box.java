60ab1c9f70e675e362f883aba2dd2a2b6defbcfb
Your Name <you@example.com>
2017-10-27 16:29:26 +0900

    init

---
+++ T25_customview/app/src/main/java/com/example/a/t25_customview/Box.java
@@ -0,0 +1,16 @@
+package com.example.a.t25_customview;
+
+import android.graphics.PointF;
+
+/**
+ * Created by a on 2017-10-27.
+ */
+
+public class Box {
+    PointF origin;
+    PointF current;
+
+    public Box(PointF origin){
+        this.origin = origin;
+    }
+}
